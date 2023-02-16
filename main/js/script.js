52902e37def4b04cdb3d8ecbd4573c99ae15e27f
Kostya <84118935+KostVund@users.noreply.github.com>
2023-02-16 11:45:57 +0100

    Add files via upload

---
+++ main/js/script.js
@@ -0,0 +1,7 @@
+/* ================ typing animation* ================ */
+var typed= new Typed(".typing",{
+	strings:["Java programer","Web Developer","Android Developer","Frontent developer"],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
